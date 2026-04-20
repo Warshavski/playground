cdc12d738d5a89f97c024c98ca773cc0efef9025
Кузнецова Дарья Алексеевна <darya.a.kuznetsova@kuper.ru>
2026-04-20 09:37:47 +0300

    add tests

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,33 +8,22 @@ class BooksController < ApplicationController
 
   def show
     result = Books::Show.call(id: params[:id])
-    if result[:success]
-      render json: result[:book], serializer: BookSerializer
-    else
-      render json: { errors: result[:error] }, status: :not_found
-    end
+    render_book_result(result, error_status: :not_found)
   end
 
   def create
     result = Books::Create.call(book_params)
-    if result[:success]
-      render json: result[:book], serializer: BookSerializer, status: :created
-    else
-      render json: { errors: result[:error] }
-    end
+    render_book_result(result, success_status: :created, error_status: :unprocessable_entity)
   end
 
   def update
     result = Books::Update.call(params: book_params, id: params[:id])
-    if result[:success]
-      render json: result[:book], serializer: BookSerializer
-    else
-      render json: { errors: result[:error] }
-    end
+    render_book_result(result, error_status: :unprocessable_entity)
   end
 
   def destroy
     result = Books::Delete.call(id: params[:id])
+
     if result[:success]
       head :no_content
     else
@@ -43,6 +32,14 @@ class BooksController < ApplicationController
   end
 
   private
+
+  def render_book_result(result, success_status: :ok, error_status: :unprocessable_entity)
+    if result[:success]
+      render json: result[:book], serializer: BookSerializer, status: success_status
+    else
+      render json: { errors: result[:error] }, status: error_status
+    end
+  end
 
   def book_params
     params.require(:book).permit(:title, :published_in, :publisher_id, :weight, :pages_count, :description, :isbn13,
